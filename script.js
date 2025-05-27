4a880ab533fa8b39e073b74a9308cc5ca758cbc6
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-05-27 18:37:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function stringChop(str, size) {
-	if(str==null)
-		return [];
-	let arr=[];
-	for (let i = 0; i < str.length; i=i+size) 
-		{
-			arr.push(str.slice(i,i+size));
-		}	
-	
-	
+  if (str == null) return [];
+
+  let arr = [];
+  for (let i = 0; i < str.length; i += size) {
+    arr.push(str.slice(i, i + size));
+  }
+
   return arr;
 }
 
-Do not change the code below
+// Do not change the code below
 const str = prompt("Enter String.");
-const size = Number(prompt("Enter Chunk Size.")); // convert to number
+const size = Number(prompt("Enter Chunk Size."));
 alert(stringChop(str, size));
